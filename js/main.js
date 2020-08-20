76607c89162c7712eb3a44ddb55540f28f01cabb
S3N1CH <semenpeleskov@gmail.com>
2020-08-20 16:03:17 +0300

    added slider with recent projects

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 new WOW().init();
 
+var mySwiper = new Swiper(".swiper-container", {
+  speed: 400,
+
+  pagination: {
+    el: ".projects-pagination",
+    bulletClass: "projects-bullet",
+    bulletActiveClass: "projects-bullet-active",
+    clickable: true,
+  },
+});
+
 function scrollToContactMe() {
-  document.querySelector("#anchor").scrollIntoView();
+  document.querySelector(".footer").scrollIntoView();
 }
